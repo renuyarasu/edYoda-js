9bbea1ee38b464c7af785117ca7faff825882798
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-21 13:49:47 +0530

    Numbers - toFixed()

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ console.clear();
 
 // In-Built Number Functions
 
-let num_01 = '10';
-let num_02 = '11.00041';
-console.log(typeof (num_01));
-console.log(typeof (num_02));
+let float_num = 87.0123546;
+console.log(typeof (float_num));
 
-let result = parseInt(num_02);
-console.log(typeof (result)); // number
-console.log(result); // 11
+let result = float_num.toFixed();
+console.log(typeof (result)); // string
+console.log(result); // 87
+
+console.log(result(2));
+console.log(result(5));
+console.log(result(10));
