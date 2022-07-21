ec0c5dcb23f5ef8155610c6296cecd0a3a518cc1
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-21 13:40:15 +0530

    Numbers - Infinity

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ let num_01 = 10; // Integer Number
 let num_02 = 10.00041; // Decemal Number
 console.log(typeof (num_01));
 console.log(typeof (num_02));
+
+let zero = num_01 / 0;
+console.log(zero); // Infinity
