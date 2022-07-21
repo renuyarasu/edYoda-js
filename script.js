1eb9af68514b735d10dfe8bd5f148915d870420b
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-21 13:39:58 +0530

    Numbers

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // JavaScript - Basics to Advanced - EdYoda Digital University, Qaifi Khan
 
 console.clear();
+
+// In-Built Number Functions
+
+let num_01 = 10; // Integer Number
+let num_02 = 10.00041; // Decemal Number
+console.log(typeof (num_01));
+console.log(typeof (num_02));
