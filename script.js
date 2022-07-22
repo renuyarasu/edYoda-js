bdec1aefe78a1dc6feefc20b7d0bb8f90b36ae16
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-22 13:40:08 +0530

    Functions

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // JavaScript - Basics to Advanced - EdYoda Digital University, Qaifi Khan
 
 console.clear();
+
+function mSum() {
+    let num_01 = 10;
+    let num_02 = 20;
+    let sum = num_01 + num_02;
+    return sum
+}
+
+console.log(mSum());
