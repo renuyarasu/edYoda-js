2ce0ddff0f20eb6a3088f5582bb14dca74b3516b
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-26 21:10:34 +0530

    Object Oriented Programming

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 // JavaScript - Basics to Advanced - EdYoda Digital University, Qaifi Khan
 
 console.clear();
+// OOPs
+let video = {
+    name: 'Intro JS',
+    format: 'mp4',
+    duration: '12.52',
+    owner: 'VedaGna',
+    getVideoName: function () {
+        return this.name;
+    },
+    getFormat: function () {
+        return this.format
+    },
+    getDuration: function () {
+        return this.duration
+    },
+    getOwner: function () {
+        return this.owner
+    }
+}
+console.log(video.getVideoName()); // Intro JS
+console.log(video.getFormat()); // mp4
+console.log(video.getDuration()); // 12.52
+console.log(video.getOwner()); // VedaGna
