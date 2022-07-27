c4264dab4f6a9d5578e62b899d93b1e5fc2a0999
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-27 17:43:20 +0530

    Rest Operator

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // JavaScript - Basics to Advanced - EdYoda Digital University, Qaifi Khan
-
 console.clear();
-// ES6 - Template Strings
 
-const name = 'VedaGna';
 
-function greet() {
-    return `Welcome ${name}`;
+// ES6 - Rest Operator
+
+const sum = (num0, num2, ...args) => {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[1]
+    }
+    console.log(sum);
 }
-console.log(greet());
+sum(1, 2, 3, 4, 5); //12
