493fb7345cec0f195c19434addaa5cc7cf61299b
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-27 16:49:05 +0530

    Template Strings

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // JavaScript - Basics to Advanced - EdYoda Digital University, Qaifi Khan
 
 console.clear();
+// ES6 - Template Strings
+
+
+const name = 'VedaGna';
+const lastname = 'Yarasu';
+
+function getFullName() {
+    return `${name} ${lastname}`
+}
+console.log(getFullName());
